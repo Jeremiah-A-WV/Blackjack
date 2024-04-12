9dee5d3959dd938ebfe5930e09a7713bff66779f
Jeremiah Whitehurst <jalexander-wv@protonmail.com>
2024-04-12 13:23:21 -0400

    Made blackjackworld bigger

--- BlackjackWorld.java
+++ BlackjackWorld.java
@@ -16,6 +16,6 @@ public class BlackjackWorld extends World
     public BlackjackWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1100, 800, 1); 
     }
 }
