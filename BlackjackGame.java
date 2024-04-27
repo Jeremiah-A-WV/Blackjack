c4242efb56e4a0336c5673a9185bf45fd17df5d0
kaylademercado <kmdemercado23@students.desu.edu>
2024-04-27 11:19:34 -0400

    new branch

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -11,15 +11,16 @@ public class BlackjackGame extends CardGame
     private BlackjackHand[] hands;
     private BlackjackHand dealer;
     int numberOfHands;
-    
+
     public BlackjackGame(int numberOfDecks, int maxNumberOfHands)
     {
-        super(numberOfDecks);    
+        super(numberOfDecks);
         hands = new BlackjackHand[maxNumberOfHands];
         dealer = new BlackjackHand("Dealer");
         numberOfHands = 0;
+
     }
-    
+
     public void join(String name)
     {
         if (numberOfHands < hands.length){
@@ -27,7 +28,7 @@ public class BlackjackGame extends CardGame
             numberOfHands++;
         }
     }
-    
+
     public void leave(String name)
     {
         boolean isFound = true;
@@ -45,11 +46,35 @@ public class BlackjackGame extends CardGame
             hands[numberOfHands] = null;
         }
     }
-    
-    public void deal(){
-        // Implement your code here
+
+    public void deal()
+    {
+        int cardsToDeal = 2; 
+
+        int[] xPositions = {65, 178, 325, 467, 617, 725};
+        int[] yPositions = {290, 399, 447, 447, 380, 273};
+        int[] rotations = {45, 35, 10, -10, -35, -45};
+
+        
+        for (int i = 0; i < numberOfHands; i++) { // dealing cards to players 
+            for (int j = 0; j < cardsToDeal; j++) {
+                Card card = getGameDeck().deal(true); 
+                card.setRotation(rotations[i]); 
+                card.setLocation(xPositions[i], yPositions[i]); 
+                hands[i].add(card);
+            }
+        }
+
+        
+        for (int i = 0; i < cardsToDeal; i++) { //// dealing cards to dealer
+            Card card = getGameDeck().deal(i == 0); // the first card face down, the second face up
+            card.setRotation(rotations[numberOfHands]); 
+            card.setLocation(xPositions[numberOfHands], yPositions[numberOfHands]);
+            dealer.add(card);
+        }
+
     }
-    
+
     /**
      * Act - do whatever the BlackJackGame wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
